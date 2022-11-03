eff98d2b862368123dc1523fe0c2257f53ddae5f
Frederic Brodbeck <der.froedi@gmail.com>
2022-11-03 22:09:11 +0100

    updated shell version

--- toggle.sh
+++ toggle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# TODO: find a nicer way to to this:
+# TODO: is there a nicer way to to this?
 gnome-extensions enable application-windows-overview@github.freder
 gnome-extensions disable application-windows-overview@github.freder
